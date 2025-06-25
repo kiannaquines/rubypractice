8a915009cd2313c46507261e3ddefe92165f8309
kiannaquines <kjgnaquines@gmail.com>
2025-06-25 23:12:42 +0800

    practice the syntax for ruby

---
+++ main.rb
@@ -0,0 +1,41 @@
+# Ruby is a developer programming language.
+# Developed by: Kian Naquines
+# Date: 23/06/2025
+
+class Vehicle
+    attr_accessor :horse_power, :fuel_tank_number
+
+    def initialize(horse_power, fuel_tank_number)
+        @horse_power = horse_power
+        @fuel_tank_number = fuel_tank_number
+    end
+
+    def drive
+        puts "I am driving the card!"
+    end
+end
+
+puts "This is the main program"
+
+name = "Kian Naquines"
+puts name
+
+pi = 3.14156
+puts pi
+
+is_active = true
+puts is_active
+
+age = 22
+puts age
+puts "this is the last part of the main program"
+v = Vehicle.new(123,123)
+v.drive
+
+END {
+    puts "this is the end of the program"
+}
+
+BEGIN {
+    puts "this is the beginning of the program"
+}
